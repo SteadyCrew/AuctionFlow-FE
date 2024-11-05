791497d435efc7dce6d465e042d3236bf1a9b3b6
gka0903 <ham3252@naver.com>
2024-11-05 10:23:59 +0900

    refactoring

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import {AuthProvider} from './components/Auth/AuthContext';
+import AppNavigator from '../AppNavigator';
+
+const App = () => {
+  return (
+    // <AuthProvider>
+    <AppNavigator />
+    // </AuthProvider>
+  );
+};
+
+export default App;
